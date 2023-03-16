0b9fc07394991819c4f48d5684afb1e0bf802945
Kerimov056 <ulvisk@code.edu.az>
2023-03-16 05:44:28 +0400

    Extension

---
+++ Extension. Indexer. Enum/Extension/MatchCountExtension.cs
@@ -0,0 +1,41 @@
+﻿namespace Extension._Indexer._Enum.Extension;
+
+public static class MatchCountExtension
+{
+    public static int MatchCount(this string sentence, string rumor)
+    {
+        int count = 0;
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (sentence[i] == ' ')
+            {
+                count++;
+            }
+        }
+
+        int say = 0;
+        char separator = ' ';
+        string word = "";
+        string[] arr = new string[count + 1];
+        int a = 0;
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (sentence[i] == separator)
+            {
+                arr[a] = word;
+                a++;
+                word = "";
+                continue;
+            }
+            word += sentence[i];
+        }
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == rumor)
+            {
+                say++;
+            }
+        }
+        return say;
+    }
+}
---
+++ Extension. Indexer. Enum/Program.cs
@@ -0,0 +1,9 @@
+﻿using Extension._Indexer._Enum.Extension;
+
+string sentence = "ne var ne yox necesen?";
+Console.WriteLine(sentence.MatchCount("ne"));
+
+string sentence1 = "her sonun bir evveli her evvelin bir sonu var";
+Console.WriteLine(sentence1.MatchCount("bir"));
+
+
